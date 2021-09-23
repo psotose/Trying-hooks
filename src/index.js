8f9e90ea700d038c4a507805f6641641acdedb16
Patricia Soto <patricia.soto@routinguc.com>
2021-09-23 11:30:36 -0300

    useState hook

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HookApp } from './HookApp';
+import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { CounterApp } from './components/01-useState/CounterApp';
+// import { HookApp } from './HookApp';
 
 ReactDOM.render(
-    <HookApp />,
+    <CounterWithCustomHook />,
   document.getElementById('root')
 );
